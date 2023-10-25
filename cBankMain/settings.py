3fd3bd463ccce12d70b4d0ba619082b0df864a20
alistdktrue2 <alistdktrue2@gmail.com>
2023-10-25 18:51:12 -0500

    requeriments cc3

--- cBankMain/settings.py
+++ cBankMain/settings.py
@@ -120,15 +120,13 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-
-
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'static','media')
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+#MEDIA_ROOT = os.path.join(BASE_DIR,'static','media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 
 
 
