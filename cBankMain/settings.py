59372730250dc419f7f9b59cec44af705abb1fef
alistdktrue2 <alistdktrue2@gmail.com>
2023-10-30 11:08:21 -0500

    requeriments dd46

--- cBankMain/settings.py
+++ cBankMain/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Configuración para archivos estáticos y multimedia
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = [os.path.join(BASE_DIR, 'staticfiles')]
 
 
 
@@ -131,7 +131,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Ruta de acceso a la carpeta de a
 
 # Configuración para archivos estáticos en producción
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
