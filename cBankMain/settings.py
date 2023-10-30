85d44859a42e2ade110a8fb4d3701d05e46e6834
alistdktrue2 <alistdktrue2@gmail.com>
2023-10-30 15:56:19 -0500

    requeriments dd50

--- cBankMain/settings.py
+++ cBankMain/settings.py
@@ -118,8 +118,9 @@ USE_TZ = True
 
 # Configuración para archivos estáticos y multimedia
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = BASE_DIR / "static/"
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 
 
@@ -131,6 +132,7 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
