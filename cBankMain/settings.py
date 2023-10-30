21e592854781b0f4f518572020ea025d3d1ecada
alistdktrue2 <alistdktrue2@gmail.com>
2023-10-30 17:16:46 -0500

    requeriments dd51

--- cBankMain/settings.py
+++ cBankMain/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 # Configuración para archivos estáticos y multimedia
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "static/"
+STATIC_ROOT = BASE_DIR / "static"
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
